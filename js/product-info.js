9bce9c70b6804ca71a4416e61f984ff3d44af726
marianoelcorrea <110739674+marianoelcorrea@users.noreply.github.com>
2022-09-08 22:24:58 -0300

    Entrega 3 ejercicio 2 terminado

--- js/product-info.js
+++ js/product-info.js
@@ -9,52 +9,62 @@ emailNavBar.innerHTML = emailValor;
 // cuando terminan de cargar los recursos de la pagina se dispara esta funcion - Primer evento -  entrega 3  ej1
 let productInfo = null;
 
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
     // se obtiene la variable local de la categoria seleccionada
     let productId = localStorage.getItem("prodID");
     // Concateno las constantes de la URL con la variable correspondiente - entrega 3
     let productInfoURL = PRODUCT_INFO_URL + productId + EXT_TYPE;
-   // obtiene los productos de la url brindada en la letra
-    getJSONData(productInfoURL).then(function(resultObj){
-        if (resultObj.status === "ok"){
+    // obtiene los productos de la url brindada en la letra
+    getJSONData(productInfoURL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
             // si la llamada fue exitosa se asigna el resultado a la variable currentProduct
-        productInfo = resultObj.data;
-        showProductInfo();  
+            productInfo = resultObj.data;
+            showProductInfo();
         }
-    })});
+    })
+});
 
 
-    // Funcion que nos permite mostrar los productos que estan en product-info.html
+// Funcion que nos permite mostrar los productos que estan en product-info.html - entrega 3 ej2
 
-    function showProductInfo(){
+function showProductInfo() {
 
-        // se crea una variable para ir escribiendo el html de la lista 
-        let htmlContentToAppend = "";
-        //Lo uso para recorrer las imagenes
-        for(let i = 0; i < productInfo.images.length; i++){    
-            // guardo en otra variable cada imagen que estoy recorriendo
-            let image = productInfo.images[i];  
-       
-                // por cada producto se agrega un item de grupo a la lista html
-                
-                htmlContentToAppend += `
-                <div onclick="setProdID(${productInfo.id})" class="list-group-item list-group-item-action cursor-active">
-                    <div class="row">
-                        <div class="col-3">
-                            <img src="${image}" alt="${productInfo.description}" class="img-thumbnail">
-                        </div>
-                        <div class="col">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h4 class="mb-1">${productInfo.name} - ${productInfo.currency} ${productInfo.cost}</h4>
-                                <small class="text-muted"> ${productInfo.soldCount} vendidos </small>
-                            </div>
-                            <p class="mb-1">${productInfo.description}</p>
-                        </div>
-                    </div>
+     // se crea una variable para ir escribiendo el html de la lista 
+     let htmlContentToAppend = "";
+
+    htmlContentToAppend += ` 
+                <div>
+                <h2> ${productInfo.name} </h2>
+                <p><b>Precio</b></p>
+                <p small>${productInfo.currency}${productInfo.cost}</p>
+                <p><b>Descripción</b></p>
+                <p small>${productInfo.description}</p>
+                <p><b>Categoria</b></p>
+                <p small>${productInfo.category}</p>
+                <p><b>Cantidad de vendidos</b></p>
+                <p small> ${productInfo.soldCount} </p>
+                <p><b>Imagenes ilustrativas</b></p>
                 </div>
+                <div class="row"> 
+    `
+    //Lo uso para recorrer las imagenes
+    for (let i = 0; i < productInfo.images.length; i++) {
+        // guardo en otra variable cada imagen que estoy recorriendo
+        let image = productInfo.images[i];
+
+        // por cada producto se agrega un item de grupo a la lista html
+
+        htmlContentToAppend += `
+
+                        <div class="col-3" >
+                          <img src="${image}" alt="${productInfo.description}" class="img-thumbnail" >
+                        </div>   
+                    
                 `
-        }
-            // Con esta funcion agarro un elemento del html por su ID, en este caso es un div, y modifico el texto que se muestra dentro del mismo en el html
-            document.getElementById("prod-info-container").innerHTML = htmlContentToAppend;
-            
     }
+    htmlContentToAppend += ` </div> `;
+
+    // Con esta funcion agarro un elemento del html por su ID, en este caso es un div, y modifico el texto que se muestra dentro del mismo en el html   - entrega 3 ej 2
+    document.getElementById("prod-info-container").innerHTML = htmlContentToAppend;
+
+}
